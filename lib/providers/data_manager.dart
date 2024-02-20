6b5b2a04d1061dc458b0bd97fcd920a062c8eec0
ndoye Philip Ndula <ndoyephilipndula@gmail.com>
2024-02-20 04:00:45 +0100

    added better features for gemini

--- lib/providers/data_manager.dart
+++ lib/providers/data_manager.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hospital/pages/chat/gemini_chat.dart';
+import 'package:hospital/utils/globals.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -26,51 +29,75 @@ class GeminiManager {
 
   Future _onCreate(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE IF NOT EXISTS data (
+      CREATE TABLE IF NOT EXISTS chats (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        data TEXT NOT NULL,
-        query TEXT NOT NULL,
-        timestamp DATETIME NOT NULL
+        content TEXT NOT NULL,
+        sentAt int,
+        image TEXT NULL,
+        isMe int NOT NULL
       )
     ''');
   }
 
-  Future<void> insertData(String data, String query) async {
+  Future<void> insertMsg(String content,
+      {String image = '', int isMe = 1}) async {
     final db = await database;
-    await db.insert('data', {
-      'data': data,
-      'query': query,
-      'timestamp': DateTime.now().toIso8601String(),
-    });
+    await db
+        .insert('chats', {
+          'content': content,
+          'sentAt': DateTime.now().millisecondsSinceEpoch,
+          "image": image,
+          "isMe": isMe
+        })
+        .then((value) => debugPrint("$value"))
+        .catchError((onError) {
+          toast(message: "error inserting chat");
+          debugPrint("Error: $onError");
+        });
   }
 
-  Future<void> updateData(int id, String data, String query) async {
+  Future<void> updateData(int id, String content,
+      {String image = '', int isMe = 1}) async {
     final db = await database;
     await db.update(
-        'data',
+        'chats',
         {
-          'data': data,
-          'query': query,
-          'timestamp': DateTime.now().toIso8601String(),
+          'content': content,
+          'sentAt': DateTime.now().millisecondsSinceEpoch,
+          "image": image,
+          "isMe": isMe
         },
         where: 'id = ?',
         whereArgs: [id]);
   }
 
-  Future<Map<String, dynamic>?> selectData(int id) async {
+  loadChats() async {
     final db = await database;
-    final result = await db.query('data', where: 'id = ?', whereArgs: [id]);
+    var data = await db.query("chats");
+    List<Message> msg = [];
+    for (Map<String, dynamic> item in data) {
+      msg.add(Message(
+          isCurrentUser: item['isMe'] == 1,
+          content: item['content'],
+          sentAt: DateTime.fromMillisecondsSinceEpoch(item['sentAt'])));
+    }
+    return msg;
+  }
+
+  Future<Map<String, dynamic>?> selectMsg(int id) async {
+    final db = await database;
+    final result = await db.query('chats', where: 'id = ?', whereArgs: [id]);
     return result.isNotEmpty ? result.first : null;
   }
 
   Future<List<Map<String, dynamic>>> selectAllData() async {
     final db = await database;
-    final result = await db.query('data');
+    final result = await db.query('chats');
     return result;
   }
 
   Future<void> deleteData(int id) async {
     final db = await database;
-    await db.delete('data', where: 'id = ?', whereArgs: [id]);
+    await db.delete('chats', where: 'id = ?', whereArgs: [id]);
   }
 }
